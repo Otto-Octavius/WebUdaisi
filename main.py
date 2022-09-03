8d4fcea23fa94604f36138718d93edc570ad8330
Sam <96664416+Otto-Octavius@users.noreply.github.com>
2022-09-03 10:26:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 
 def geocoder(string):
+    '''
+    .
+    This function takes in a string and returns the raw location. That is every possible detail of the location. 
+    For example: The cordinates, the type and elevation.
+
+    :param str string: The string containing a vague address
+    
+    :return: Returns a geopy.location dictionary
+    '''
     locator = Nominatim(user_agent="myGeocoder")
     return locator.geocode(string)
 
 def geocoder_test(string):
+      '''
+    .
+    This function does the same as above but reserved for API calls and Testing(Without Streamlit UI involved)
+    '''
     locator = Nominatim(user_agent="myGeocoder")
     location = locator.geocode(string)
     if not location:
@@ -15,6 +28,11 @@ def geocoder_test(string):
         return(location.address)
     
 def st_ui():
+    '''
+    .
+    This function creates a Streamlit UI and performs the task of getting in the string and having a button that triggers the above the function. If found
+    this also visualises the location through Streamlit's map function.
+    '''
     st.title("The Perfect Address 🌍")
     l = st.text_input("Enter a building or location")
     location = geocoder(l)
