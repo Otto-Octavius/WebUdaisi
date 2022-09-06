f2ab0068a0355f1785e104c9aa736e64e4e8bf13
Sam <96664416+Otto-Octavius@users.noreply.github.com>
2022-09-06 23:51:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,41 @@ import streamlit as st
 import pandas as pd
 from geopy.geocoders import Nominatim
 
+
 def geocoder(string):
-    '''
+    """
     .
-    This function takes in a string and returns the raw location. That is every possible detail of the location. 
-    For example: The cordinates, the type and elevation.
+    This function takes in a string and returns the raw location. That is every possible detail of the location.
+    For example: The coordinates, the type and elevation.
 
     :param str string: The string containing a vague address
-    
+
     :return: Returns a geopy.location dictionary
-    '''
+    """
     locator = Nominatim(user_agent="myGeocoder")
     return locator.geocode(string)
 
+
 def geocoder_test(string):
-      '''
-    .
-    This function does the same as above but reserved for API calls and Testing(Without Streamlit UI involved)
-    '''
+    """
+  .
+  This function does the same as above but reserved for API calls and Testing(Without Streamlit UI involved)
+  """
+
     locator = Nominatim(user_agent="myGeocoder")
     location = locator.geocode(string)
     if not location:
-        return('Add more Info (For example: Road, City, District)')
+        return 'Add more Info (For example: Road, City, District)'
     else:
-        return(location.address)
-    
+        return location.address
+
+
 def st_ui():
-    '''
+    """
     .
     This function creates a Streamlit UI and performs the task of getting in the string and having a button that triggers the above the function. If found
     this also visualises the location through Streamlit's map function.
-    '''
+    """
     st.title("The Perfect Address 🌍")
     l = st.text_input("Enter a building or location")
     location = geocoder(l)
@@ -46,8 +50,10 @@ def st_ui():
             df = pd.DataFrame(data, columns=['lat', 'lon'])
             st.map(df)
             st.markdown(
-                "<h4 style='text-align: center; color: black;'> Incorrect location? Give it a retry with more info </h4>",
+                "<h4 style='text-align: center; color: black;'> Incorrect location? Give it a retry with more info "
+                "</h4>",
                 unsafe_allow_html=True)
+
 
 if __name__ == '__main__':
     st_ui()
